fb17788e8f7f9606c893b2ffb5b5d336259b6abe
ARgits <33343265+ARgits@users.noreply.github.com>
2024-02-19 16:40:43 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
-supabaseUrl:https://zrzdptljtpgfutrzaqfq.supabase.co,
-supabaseKey:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyemRwdGxqdHBnZnV0cnphcWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQyNjAyMzksImV4cCI6MTk3OTgzNjIzOX0.5cddPWXKD2OV_A-ORc8LVTsEuZHt88Dbtxr1Y2XsUjQ
 
 };
